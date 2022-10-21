b485616e3a147c077fcdb19dcd64fe81ee9e7118
Narek <narekazibekyan5@gmail.com>
2022-10-22 01:17:44 +0400

    Updated Task 1

--- Homework1/Task1.js
+++ Homework1/Task1.js
@@ -2,8 +2,24 @@
 
 "use strict";
 
-let firstAngle = 45;
-let secondAngle = 90;
+// let firstAngle = 45;
+// let secondAngle = 90;
+
+// let thirdAngle = 180 - firstAngle - secondAngle;
+// console.log("Third angle = " + thirdAngle);
+
+
+
+// let firstAngle = 30;
+// let secondAngle = 30;
+
+// let thirdAngle = 180 - firstAngle - secondAngle;
+// console.log("Third angle = " + thirdAngle);
+
+
+
+let firstAngle = 75;
+let secondAngle = 25;
 
 let thirdAngle = 180 - firstAngle - secondAngle;
 console.log("Third angle = " + thirdAngle);
