bd56315707736e57795b289b9f1c39739a9c2d20
Narek <narekazibekyan5@gmail.com>
2022-11-04 20:25:21 +0400

    Updated Task 4

--- Homework6/Task4.js
+++ Homework6/Task4.js
@@ -2,15 +2,14 @@
 
 let firstValue = +prompt("Enter first value");
 let secondValue = +prompt("Enter second value");
-while (
-   firstValue < 10 ||
-   firstValue > 30 ||
-   secondValue < 10 ||
-   secondValue > 30
-) {
-   alert("The numbers don't match the conditions. Import the numbers again.");
-   firstValue = +prompt("Enter first value");
-   secondValue = +prompt("Enter second value");
+
+while (firstValue < 10 || firstValue > 30) {
+   alert("The first number does not match the conditions.");
+   firstValue = +prompt("Enter again:");
+}
+while (secondValue < 10 || secondValue > 30) {
+   alert("The second number does not match the conditions.");
+   secondValue = +prompt("Enter again:");
 }
 
 let greaterValue = Math.max(firstValue, secondValue);
