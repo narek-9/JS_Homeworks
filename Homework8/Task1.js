1236822cb2571ff7eddea35db1abf7369f6b9c25
Narek <narekazibekyan5@gmail.com>
2022-11-07 19:48:25 +0400

    Add Task 1

---
+++ Homework8/Task1.js
@@ -0,0 +1,27 @@
+// Write a function that takes a natural number n as an argument and returns:
+// a) how many times does the first digit occur in it,
+
+// a
+let n = +prompt("Enter natural number"),
+   array = [];
+while (isNaN(n)) {
+   n = +prompt("Enter natural number again");
+}
+
+while (n !== 0) {
+   let lastDigit = n % 10;
+   n = (n - lastDigit) / 10;
+   array.unshift(lastDigit);
+}
+
+function getCount(array) {
+   let count = 0;
+   for (let i = 0; i < array.length; i++) {
+      if (array[i] === array[0]) {
+         count++;
+      }
+   }
+   return array[0] + " occurs " + count + " times";
+}
+
+console.log(getCount(array));
