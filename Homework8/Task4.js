09b2adcbc6cae220eb914084d08f8eaec9232f27
Narek <narekazibekyan5@gmail.com>
2022-11-08 16:57:56 +0400

    Add Task 4

---
+++ Homework8/Task4.js
@@ -0,0 +1,30 @@
+// Write a function that takes a natural number n as an argument and returns:
+// d) true if it contains duplicate digits, and returns false otherwise.
+
+
+
+let n = +prompt("Enter natural number again"),
+   array = [];
+while (isNaN(n)) {
+   n = +prompt("Enter natural number again");
+}
+
+while (n !== 0) {
+   let lastDigit = n % 10;
+   n = (n - lastDigit) / 10;
+   array.unshift(lastDigit);
+}
+
+function checkDuplicate(array) {
+   while (array.length !== 1) {
+      let a = array[0];
+      array.shift();
+      if (array.indexOf(a) !== -1) {
+         return true;
+      } else if (array.length === 1 && array.indexOf(a) === -1) {
+         return false;
+      }
+   }
+}
+
+console.log(checkDuplicate(array));
