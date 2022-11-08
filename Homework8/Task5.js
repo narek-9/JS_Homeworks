9aeb146ed157fe8c5ac549f417dfb4aed95faba1
Narek <narekazibekyan5@gmail.com>
2022-11-08 19:38:23 +0400

    Add Task 5

---
+++ Homework8/Task5.js
@@ -0,0 +1,25 @@
+// Write a function that takes a natural number n as an argument and returns:
+// e) index of the largest digit (if this digit occurs several times in the number, return the leftmost index).
+
+let n = +prompt("Enter natural number"),
+   array = [];
+while (isNaN(n)) {
+   n = +prompt("Enter natural number again");
+}
+
+while (n !== 0) {
+   let lastDigit = n % 10;
+   n = (n - lastDigit) / 10;
+   array.unshift(lastDigit);
+}
+
+function getMaxDigitIndex(array) {
+   let maxNumber = array[0];
+   for (let i = 1; i < array.length; i++) {
+      if (array[i] > maxNumber) {
+         maxNumber = array[i];
+      }
+   }
+   return array.indexOf(maxNumber);
+}
+console.log(getMaxDigitIndex(array));
