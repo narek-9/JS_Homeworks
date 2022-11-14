ab75e7edb44eae4cac2336af77fa279b3f5d09f5
Narek <narekazibekyan5@gmail.com>
2022-11-14 19:11:40 +0400

    Add Task 1

---
+++ Homework10/Task1.js
@@ -0,0 +1,19 @@
+// Write a recursive function that takes and m numbers and returns an array filled with all numbers in the range (n, m).
+
+let n = +prompt("Enter number n");
+let m = +prompt("Enter number m");
+
+function getArray(n, m, array) {
+   if (m > n) {
+      array.push(n);
+      return getArray(n + 1, m, array);
+   } else if (n > m) {
+      array.push(n);
+      return getArray(n - 1, m, array);
+   } else {
+      array.shift();
+      return array;
+   }
+}
+
+getArray(n, m, []);
