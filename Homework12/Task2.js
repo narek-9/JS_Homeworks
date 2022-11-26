2d2a5270548e1df9326507cdee52d191cde143ee
Narek <narekazibekyan5@gmail.com>
2022-11-26 11:05:00 +0400

    Task 2

--- Homework12/Task2.js
+++ Homework12/Task2.js
@@ -1,3 +1,8 @@
+// Create a "rock-paper-scissors" game where the computer will keep random
+// one of the rock, scissors, or paper values ​​and depends on user's pormpt
+// "You win!", "You loose..." or "Draw" will be printed from the entered value. Make user
+// validation of the value imported by
+
 const rockPaperScissors = (userValue) => {
    let playAgain = true;
    while (playAgain) {
