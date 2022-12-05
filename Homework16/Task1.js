626174886b0974700e69b8c437e5a8438ab89fc2
Narek <narekazibekyan5@gmail.com>
2022-12-06 00:31:28 +0400

    Upd Task 1

--- Homework16/Task1.js
+++ Homework16/Task1.js
@@ -1,59 +1,45 @@
-const multipleActions = (arr, actionType, i = null) => {
-   //    const validate = (arr, i) => {
-   //       if (!Array.isArray(arr)) {
-   //          return false;
-   //       } else if (
-   //          typeof i !== "number" ||
-   //          i < 0 ||
-   //          !Number.isInteger(i) ||
-   //          isNaN(i)
-   //       ) {
-   //          return false;
-   //       }
-   //       return true;
-   //    };
-   //    if (validate()) {
-   switch (actionType) {
-      case "Primary sum":
-         let sum = 0;
-         for (let j = 0; j < i; j++) {
-            sum += arr[j];
-         }
-         return sum;
-      case "Secondary sum":
-         let sumValues = 0;
-         for (let j = arr.length - 1; j > i; j--) {
-            sumValues += arr[j];
-         }
-         return sumValues;
-      case "Array with smaller index values":
-         let newArray = [];
-         for (let j = 0; j < i; j++) {
-            newArray.push(arr[j]);
-         }
-         return newArray;
-      case "Array values with zero remainder":
-         let newArr = [];
-         for (let j = 0; j < arr.length; j++) {
-            if (arr[j] % i === 0) {
-               newArr.push(arr[j]);
+const multipleActions = (arr, i, actionType) => {
+   if (validate(arr, i, actionType)) {
+      switch (actionType) {
+         case "Primary sum":
+            let sum = 0;
+            for (let j = 0; j < i; j++) {
+               sum += arr[j];
             }
-         }
-         return newArr;
-
-      case "Array with square values":
-         let array = [];
-         for (let j = 0; j < arr.length; j++) {
-            array.push(arr[j] ** 2);
-         }
-         return array;
-      default:
-         return arr;
+            return sum;
+         case "Secondary sum":
+            let sumValues = 0;
+            for (let j = arr.length - i; j < arr.length; j++) {
+               sumValues += arr[j];
+            }
+            return sumValues;
+         case "Array with smaller index values":
+            arr.splice(i, arr.length - 1);
+            return arr;
+         case "Array values with zero remainder":
+            return arr.filter((value) => !(value % i));
+         case "Array with square values":
+            arr.forEach((value, index) => (arr[index] = value ** 2));
+            return arr;
+         default:
+            return arr;
+      }
    }
-   //    }
+   return "Enter valid values";
 };
 
 const arr = [1, 2, 3, 4, 5];
-let index = 4,
-   actionType = "Array with smaller index values";
-console.log(multipleActions(arr, actionType, index));
+let index = 3,
+   actionType = "Array values with zero remainder";
+
+const validate = (arr, i, actionType) => {
+   if (!Array.isArray(arr)) return false;
+   else if (!Number(i) || !Number.isInteger(i) || i > arr.length) {
+      return false;
+   } else if (typeof actionType !== "string" || actionType.length === 0) {
+      return false;
+   }
+   return true;
+};
+
+console.log(multipleActions(arr, index, actionType));
