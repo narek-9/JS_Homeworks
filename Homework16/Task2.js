67b0629d4e6ef2ce0115eec1322f8a2185450c81
Narek <narekazibekyan5@gmail.com>
2022-12-06 14:30:08 +0400

    Upd Task 2

--- Homework16/Task2.js
+++ Homework16/Task2.js
@@ -5,17 +5,10 @@
 // numbers in this array".
 
 const firstOddNumber = (arr, i) => {
-   if (Array.isArray(arr)) {
-      if (arr.length) {
-         if (i === arr.length - 1) {
-            return !(arr[i] % 2)
-               ? "There are no odd numbers in this array"
-               : arr[i];
-         } else if (arr[i] % 2) return arr[i];
-         return firstOddNumber(arr, i + 1);
-      }
-      return "There are no values in this array";
-   }
-   return `${arr} is not an array`;
+   return i === arr.length
+      ? "There are no odd numbers in this array"
+      : arr[i] % 2
+      ? arr[i]
+      : firstOddNumber(arr, i + 1);
 };
-console.log(firstOddNumber([2, 14, 6, 4, 9, 10, 1, 5, 8], 0));
+console.log(firstOddNumber([4, 10, 6, 8, 4, 6, 8], 0));
