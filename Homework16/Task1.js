3197d98994be3f2f12fca218fc129fdacc23a05b
Narek <narekazibekyan5@gmail.com>
2022-12-06 14:29:53 +0400

    Upd Task 1

--- Homework16/Task1.js
+++ Homework16/Task1.js
@@ -1,26 +1,24 @@
 const multipleActions = (arr, i, actionType) => {
    if (validate(arr, i, actionType)) {
+      let sum = 0;
       switch (actionType) {
          case "Primary sum":
-            let sum = 0;
             for (let j = 0; j < i; j++) {
                sum += arr[j];
             }
             return sum;
          case "Secondary sum":
-            let sumValues = 0;
             for (let j = arr.length - i; j < arr.length; j++) {
-               sumValues += arr[j];
+               sum += arr[j];
             }
-            return sumValues;
+            return sum;
          case "Array with smaller index values":
             arr.splice(i, arr.length - 1);
             return arr;
          case "Array values with zero remainder":
             return arr.filter((value) => !(value % i));
          case "Array with square values":
-            arr.forEach((value, index) => (arr[index] = value ** 2));
-            return arr;
+            return arr.map((value) => value ** 2);
          default:
             return arr;
       }
@@ -29,12 +27,12 @@ const multipleActions = (arr, i, actionType) => {
 };
 
 const arr = [1, 2, 3, 4, 5];
-let index = 3,
-   actionType = "Array values with zero remainder";
+let index = 2,
+   actionType = "Secondary sum";
 
 const validate = (arr, i, actionType) => {
    if (!Array.isArray(arr)) return false;
-   else if (!Number(i) || !Number.isInteger(i) || i > arr.length) {
+   else if (!Number(i) || !Number.isInteger(i) || i > arr.length || i < 0) {
       return false;
    } else if (typeof actionType !== "string" || actionType.length === 0) {
       return false;
