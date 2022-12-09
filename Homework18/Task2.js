1c67ea658f44dd61a915b364ca9fdc4031648ad9
Narek <narekazibekyan5@gmail.com>
2022-12-09 20:54:02 +0400

    Add Task 2

---
+++ Homework18/Task2.js
@@ -0,0 +1,16 @@
+// Declare a sortByAge function that takes an array of users as a parameter.
+// The latter contains objects containing user data. The sortByAge function
+// should sort the users array according to the age of the users and return the result
+// mass.
+
+const sortByAge = (users) =>
+   users.sort((element1, element2) => element1.age - element2.age);
+
+console.log(
+   sortByAge([
+      { name: "Vanik", age: 21 },
+      { name: "Alex", age: 17 },
+      { name: "Nare", age: 34 },
+      { name: "Mark", age: 20 },
+   ])
+);
