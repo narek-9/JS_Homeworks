5c71d923a502bb39248aacaf2b5d509a56021b5b
Narek <narekazibekyan5@gmail.com>
2022-12-12 18:08:17 +0400

    Upd Task 1

--- Homework18/Task1.js
+++ Homework18/Task1.js
@@ -1,11 +1,7 @@
-const sum = (number1) => {
-   return (number2) => {
-      return typeof number1 === "number" &&
-         number1 !== NaN &&
-         typeof number2 === "number" &&
-         number2 !== NaN
-         ? number1 + number2
-         : "Invalid value";
-   };
-};
+const sum = (number1) => (number2) =>
+   validateNumbers(number1) && validateNumbers(number2)
+      ? number1 + number2
+      : "Invalid value";
+
+const validateNumbers = (value) => typeof value === "number" && value !== NaN;
 console.log(sum(12)(3));
