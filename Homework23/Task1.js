e126b648c105048953b73ea0d7ea24c868c4ec57
Narek <narekazibekyan5@gmail.com>
2023-01-16 18:40:34 +0400

    Add Task 1

---
+++ Homework23/Task1.js
@@ -0,0 +1,36 @@
+// ա
+
+// const checkNeedSurgery = () => {
+//    const probability = Math.round(Math.random() * 100);
+//    if (probability <= 30) {
+//       return new Promise((resolve) =>
+//          setTimeout(() => resolve("Need a surgery"), 10000)
+//       );
+//    } else {
+//       return new Promise((resolve) =>
+//          setTimeout(() => resolve("No need a surgery"), 10000)
+//       );
+//    }
+// };
+
+// checkNeedSurgery()
+//    .finally(() => alert("The answer of the test is ready..."))
+//    .then((text) => alert(text));
+
+// բ
+const checkNeedSurgery = () => {
+   const probability = Math.round(Math.random() * 100);
+   if (probability <= 30)
+      return new Promise((resolve) =>
+         setTimeout(() => resolve("Need a surgery"), 10000)
+      );
+   else {
+      return new Promise((resolve) =>
+         setTimeout(() => resolve("No need a surgery"), 10000)
+      );
+   }
+};
+
+checkNeedSurgery()
+   .finally(() => alert("The answer of the test is ready..."))
+   .then((text) => alert(text));
