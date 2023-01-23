8b4e3344e04bc6101ce3560c3de4b3f5f9a6af53
Narek <narekazibekyan5@gmail.com>
2023-01-23 22:10:35 +0400

    Upd Task 1

--- Homework 25,26/Task1.js
+++ Homework 25,26/Task1.js
@@ -1,14 +1,16 @@
 const removeFalseyValues = (val) => {
-   if (typeof val === "object" && val !== null) {
+   if (valueValidation(val)) {
       if (Array.isArray(val)) return val.filter((el) => !!el);
 
       const newObj = {};
-      for (const key of Object.entries(val)) {
-         if (key[1]) newObj[key[0]] = key[1];
+      for (const key in val) {
+         if (val[key]) newObj[key] = val[key];
       }
       return newObj;
    }
    return "Invalid data";
 };
+const valueValidation = (value) => typeof value === "object" && value !== null;
+
 console.log(removeFalseyValues([10, true, null, "", 64, "Ani"]));
 console.log(removeFalseyValues({ a: "Hello World", b: false, c: "0", d: !{} }));
