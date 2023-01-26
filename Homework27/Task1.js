c0353aa830b61ecf234da8e69c0a4e46e3453a66
Narek <narekazibekyan5@gmail.com>
2023-01-26 18:34:06 +0400

    Upd Task 1

--- Homework27/Task1.js
+++ Homework27/Task1.js
@@ -1,10 +1,10 @@
 const sliceSymb = (str, symb) => {
-   const arr = [];
+   let newStr = "";
    for (let value of str) {
       if (value.toLowerCase() !== symb.toLowerCase()) {
-         arr.push(value);
+         newStr += value;
       }
    }
-   return arr.join("");
+   return newStr;
 };
 console.log(sliceSymb("Ann is playing piano. Absolutely fantastic.", "a"));
